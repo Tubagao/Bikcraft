0f81c134aeda1a047641cc6fe956ded998920a15
Vinícius <vinnydepaula@hotmail.com>
2021-06-17 10:35:03 -0300

    finalização de formulario funcional

--- js/script.js
+++ js/script.js
@@ -17,3 +17,11 @@ nav: true
 if(window.SimpleAnime) {
  new SimpleAnime();
 }
+
+
+new SimpleForm({
+  form: ".formphp",
+  button: "#enviar",
+  erro: "<div id='form-erro'><h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></h2></div>", // mensagem de erro
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+});
